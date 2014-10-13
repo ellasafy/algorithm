fe5ec40d2fa4b4cdbfc4170bb93793f70cc8d609
ellasafy <ellasafy@outlook.com>
2014-10-13 22:28:06 +0800

    ..

--- src/main/java/com/algorithm/array/Search.java
+++ src/main/java/com/algorithm/array/Search.java
@@ -1,5 +1,17 @@
 package com.algorithm.array;
 
+import java.util.LinkedList;
+
 public class Search {
 
+	public void findchar(int[] a) {
+		LinkedList<Integer> li = new LinkedList<Integer>();
+		li.size();
+		int tmp = 0;
+		for (int i = 1; i < a.length; i++) {
+			if (a[i] == a[tmp]) {
+				tmp = i +1;
+			}
+		}
+	}
 }
--- src/main/java/com/algorithm/string/StringAll.java
+++ src/main/java/com/algorithm/string/StringAll.java
@@ -3,45 +3,74 @@ package com.algorithm.string;
 import org.junit.Test;
 
 public class StringAll {
-	
+
 	@Test
 	public void testSumOfChar() {
 		String a = "abababccccc";
 		String b = "abc";
-		
-		sumOfChar(a,b);
+
+		sumOfChar(a, b);
 	}
+
 	/**
 	 * 给定两个字符串，a，b，求a中b出现次数
+	 * 
 	 * @param orgin
 	 * @param test
 	 */
 	public void sumOfChar(String orgin, String test) {
 		char[] orginChar = orgin.toCharArray();
 		char[] testChar = test.toCharArray();
-		
-	    boolean tmp[] = new boolean[128];
-	    
-	    for (int i = 0; i < testChar.length; i++) {
-	    	tmp[testChar[i]] = true;
-	    	System.out.println(testChar[i]);
-	    }
-	    
-	    int[] sum = new int[128];
-	    
-//	    Map<String, Integer> mp = new HashMap<String, Integer>();
-	    for (int i = 0; i < orginChar.length; i++) {
-	    	if (tmp[orginChar[i]]) {
-	    		sum[orginChar[i]]++;
-	    	}
-	    }
-	    
-	    for (int i = 0; i < testChar.length; i++) {
-	    	if (sum[testChar[i]] >0) {
-	    		System.out.println(testChar[i] + " : " + sum[testChar[i]]);
-	    	}
-	    }
-	    
+
+		boolean tmp[] = new boolean[128];
+
+		for (int i = 0; i < testChar.length; i++) {
+			tmp[testChar[i]] = true;
+			System.out.println(testChar[i]);
+		}
+
+		int[] sum = new int[128];
+
+		// Map<String, Integer> mp = new HashMap<String, Integer>();
+		for (int i = 0; i < orginChar.length; i++) {
+			if (tmp[orginChar[i]]) {
+				sum[orginChar[i]]++;
+			}
+		}
+
+		for (int i = 0; i < testChar.length; i++) {
+			if (sum[testChar[i]] > 0) {
+				System.out.println(testChar[i] + " : " + sum[testChar[i]]);
+			}
+		}
+
+	}
+
+	@Test
+	public void testFindElem() {
+		String ss = "abcaefflf";
+		findElem(ss);
+		System.out.println("");
+		String sss = "abcaefflfeeecceee";
+		findElem(sss);
+	}
+
+	/**
+	 * a~z包括大小写与0~9组成的N个数 用最快的方式把其中重复的元素挑出来。
+	 * 
+	 * @param s
+	 */
+	public void findElem(String s) {
+		char[] ss = s.toCharArray();
+		int[] tmp = new int[128];
+		for (int i = 0; i < ss.length; i++) {
+			if (tmp[ss[i]] == 1) {
+				System.out.print(ss[i] + " ");
+				tmp[ss[i]]++;
+			} else {
+				tmp[ss[i]]++;
+			}
+		}
 	}
 
 }
